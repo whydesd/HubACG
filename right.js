977b38781d32b8a679823ffca89d6e7d2811a10d
白翼Desd <shi_yichen@hotmail.com>
2020-08-28 16:03:19 +0800

    Create right.js

---
+++ right.js
@@ -0,0 +1,9 @@
+live2d.init({"display": {
+      "superSample": 2,
+      "width": 200,
+      "height": 400,
+              "position": "right",
+                  "hOffset": 0,
+      "vOffset": 0
+          }
+      });
